446b843d1d423285e9c83a0044c88f0c5927f356
frawnwa12 <90830942+frawnwa12@users.noreply.github.com>
2021-09-18 19:03:45 +0200

    Add files via upload

--- BANDA.lua
+++ BANDA.lua
@@ -3748,6 +3748,7 @@ local Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '𝗗𝙀𝗩 𝗔𝗦𝙏𝙊𝙍𝙊𝗞𝗦',url="t.me/Q_0_ll"}},
+
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/Qtdao/68&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
